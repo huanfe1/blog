a36ba5d050b6b21959b546e1f9636894ea53d398
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-05-21 20:16:41 +0800

    去除mdx

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-// next.config.js
-
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-        providerImportSource: '@mdx-js/react'
-    },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     reactStrictMode: true,
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
