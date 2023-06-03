8a1107574b0a9c8baa410706865a951b8decef6c
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-06-03 16:19:35 +0800

    修改首页封面图显示规则，防止 cls

--- components/card.jsx
+++ components/card.jsx
@@ -4,9 +4,14 @@ export default function Card({ post }) {
     return (
         <Link href={'/post/' + post.abbrlink} className="block overflow-hidden rounded-xl bg-white shadow">
             {post.cover && (
-                <div className="flex max-h-80 items-center overflow-hidden">
-                    <img src={post.cover} alt={post.title} loading="lazy" />
-                </div>
+                <div
+                    className="hidden items-center overflow-hidden bg-gray-200 sm:flex sm:h-56 md:h-64 lg:h-80"
+                    style={{
+                        backgroundImage: `url(${post.cover})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                ></div>
             )}
             <div className="p-5">
                 <h2 className="mb-2 text-xl">{post.title}</h2>
