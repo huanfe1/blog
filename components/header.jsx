80f9d6ef595ed2691269b6896a27f5604c891fb7
幻非 <eg1981400335@gmail.com>
2023-06-03 21:49:22 +0800

    fix: header bug

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { nav } from '@/styles/module/header.module.scss';
 
+const development = process.env.NODE_ENV === 'development';
+
 export default function header() {
     return (
         <nav className={nav}>
@@ -11,8 +13,10 @@ export default function header() {
                 <Link href="/">首页</Link>
                 <Link href="/tags">标签</Link>
                 <Link href="/archives">归档</Link>
-                <Link href="/atom.xml" target='_blank'>订阅</Link>
-                <Link href="/admin">管理</Link>
+                <Link href="/atom.xml" target="_blank">
+                    订阅
+                </Link>
+                {development && <Link href="/admin">管理</Link>}
             </div>
         </nav>
     );
