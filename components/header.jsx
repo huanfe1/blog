e5e83f52b0e52053d29c23df77f02aca08a5dd95
幻非 <eg1981400335@gmail.com>
2023-06-04 19:15:33 +0800

    Change header rss logo

--- components/header.jsx
+++ components/header.jsx
@@ -14,12 +14,23 @@ export default function header() {
                 <Link href="/">首页</Link>
                 <Link href="/tags">标签</Link>
                 <Link href="/archives">归档</Link>
-                <Link href="/atom.xml" target="_blank">
-                    订阅
-                </Link>
                 {development && <Link href="/admin">管理</Link>}
+                <Link href="/atom.xml" target="_blank" className="flex items-center">
+                    <Rss />
+                </Link>
                 <Color />
             </div>
         </nav>
     );
 }
+
+const Rss = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" viewBox="0 0 24 24">
+            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M4 11a9 9 0 0 1 9 9M4 4a16 16 0 0 1 16 16" />
+                <circle cx="5" cy="19" r="1" />
+            </g>
+        </svg>
+    );
+};
