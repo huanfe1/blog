cf7260866213836dcc117e7dc5ef62ba6c504f36
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-06-04 17:44:20 +0800

    Add dark mode

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 export default function footer() {
     return (
-        <footer className="bg-main w-full bg-white p-6 shadow">
+        <footer className="bg-main w-full bg-[--main] p-6 shadow">
             <div className="space-x-2 text-center">
                 <span>{`Copyright © 2022 - ${dayjs().format('YYYY')}`}</span>
                 <span>All right reserved by 幻非</span>
