cdba871be02c03b15be99390ed7edbfc3dfcd74c
幻非 <eg1981400335@gmail.com>
2023-06-11 20:08:57 +0800

    fix: bug

--- utils/wordcount.js
+++ utils/wordcount.js
@@ -21,10 +21,11 @@ export const min2read = (content, { cn = 300, en = 160 } = {}) => {
 export const wordcount = content => {
     const len = counter(content);
     const count = len[0] + len[1];
-    if (count < 1000) {
-        return count;
-    }
-    return Math.round(count / 100) / 10 + 'k';
+    return count;
+    // if (count < 1000) {
+    //     return count;
+    // }
+    // return Math.round(count / 100) / 10 + 'k';
 };
 
 export const totalcount = (posts, tiny = true) => {
