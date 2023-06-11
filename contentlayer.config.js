6e9c347d368ebaecec06b8a693a5323df2fc9b3d
幻非 <eg1981400335@gmail.com>
2023-06-11 23:02:10 +0800

    update

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -6,40 +6,10 @@ import rehypeSlug from 'rehype-slug';
 import truncate from './utils/truncate';
 import { wordcount } from './utils/wordcount';
 
-export const Post = defineDocumentType(() => ({
-    name: 'Post',
-    filePathPattern: `./*.md`,
+const options = {
     fields: {
         title: { type: 'string', required: true },
         date: { type: 'string', required: true },
-        abbrlink: { type: 'string', required: true },
-        author: { type: 'string', required: true },
-        cover: { type: 'string', required: false, default: '' },
-        comments: { type: 'boolean', required: false, default: true },
-        tags: { type: 'list', of: { type: 'string' }, required: false, default: [] },
-        copyright: { type: 'boolean', required: false, default: true },
-        categories: { type: 'string', required: false, default: '' },
-        excerpt: { type: 'string', required: false, default: '' },
-    },
-    computedFields: {
-        date: { type: 'string', resolve: post => dayjs(post.date).format('YYYY-MM-DD') },
-        wordcount: { type: 'number', resolve: post => wordcount(post.body.html.replace(/<[^>]+>/g, '')) },
-        excerpt: {
-            type: 'string',
-            resolve: post => {
-                if (post.excerpt !== '') return post.excerpt;
-                return truncate(post.body.html.replace(/<[^>]+>/g, ''), { length: 120 }).replace(/[\n\r]/g, '');
-            },
-        },
-    },
-}));
-
-export const Draft = defineDocumentType(() => ({
-    name: 'Draft',
-    filePathPattern: `./drafts/*.md`,
-    fields: {
-        title: { type: 'string', required: true },
-        date: { type: 'string', required: false },
         abbrlink: { type: 'string', required: true },
         author: { type: 'string', required: false, default: '幻非' },
         cover: { type: 'string', required: false, default: '' },
@@ -60,6 +30,18 @@ export const Draft = defineDocumentType(() => ({
             },
         },
     },
+};
+
+export const Post = defineDocumentType(() => ({
+    ...options,
+    name: 'Post',
+    filePathPattern: `./*.md`,
+}));
+
+export const Draft = defineDocumentType(() => ({
+    ...options,
+    name: 'Draft',
+    filePathPattern: `./drafts/*.md`,
 }));
 
 export default makeSource({
