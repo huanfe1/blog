b7eb8118d326b345aaf4b0059975468d6b0c0ec2
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-06-24 11:04:10 +0800

    update ui

--- components/card.jsx
+++ components/card.jsx
@@ -2,12 +2,16 @@ import Link from 'next/link';
 
 export default function Card({ post }) {
     return (
-        <article className="p-5">
-            <Link href={'/post/' + post.abbrlink} className="block">
-                <h2 className="mb-2 text-2xl hover:text-[--link-hover]">{post.title}</h2>
-            </Link>
-            <section className="my-2 line-clamp-2 text-sm sm:line-clamp-none">{post.excerpt}</section>
-            <time className="time text-sm">{post.date}</time>
-        </article>
+        <div>
+            <article>
+                <Link href={'/post/' + post.abbrlink} className="block">
+                    <h2 className="mb-2 text-2xl hover:text-[--link-hover]">{post.title}</h2>
+                </Link>
+                <section className="my-2 line-clamp-2 text-sm leading-8 text-gray-600 dark:text-gray-300/75 sm:line-clamp-none">
+                    {post.excerpt}
+                </section>
+                <time className="time text-sm text-gray-600 dark:text-gray-300/75">{`发布于 ${post.date}`}</time>
+            </article>
+        </div>
     );
 }
