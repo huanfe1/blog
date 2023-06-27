75f84623933af1476bba8acb5cba57bdc53cdb51
幻非 <eg1981400335@gmail.com>
2023-06-27 13:36:09 +0800

    Style update

--- components/card.jsx
+++ components/card.jsx
@@ -18,7 +18,7 @@ export default function Card({ post }) {
                 <Link href={'/post/' + post.abbrlink} className=" mb-2 inline-block">
                     <h2 className="text-2xl hover:text-[--link-hover]">{post.title}</h2>
                 </Link>
-                <section className="my-2 line-clamp-2 text-sm leading-8 text-gray-600 dark:text-gray-300/75 sm:line-clamp-none">
+                <section className="my-2 line-clamp-2 text-sm leading-8 text-gray-700 dark:text-gray-300/75 sm:line-clamp-none">
                     {post.excerpt}
                 </section>
                 <time className="time text-sm text-gray-600 dark:text-gray-300/75">{`发布于 ${post.date}`}</time>
