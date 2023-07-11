f5131184bf8d9bb0881aa085b0fc64e4acd0fe90
幻非 <eg1981400335@gmail.com>
2023-07-11 20:36:31 +0800

    update seo

--- components/top.tsx
+++ components/top.tsx
@@ -4,7 +4,10 @@ export default function Top() {
         history.replaceState(null, null, ' ');
     };
     return (
-        <div className="fixed bottom-10 right-10 cursor-pointer rounded-xl bg-[--pop] p-2 shadow" onClick={click}>
+        <div
+            className="fixed bottom-10 right-10 cursor-pointer rounded-xl border border-[--border] bg-[--pop] p-2"
+            onClick={click}
+        >
             <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6l1.41 1.41z" />
             </svg>
