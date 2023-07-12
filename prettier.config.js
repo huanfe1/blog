de88f583032c3a4be079c29df03d24714f656408
幻非 <eg1981400335@gmail.com>
2023-07-12 12:35:45 +0800

    fix typescript bug

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [require('prettier-plugin-tailwindcss')],
-    printWidth: 120,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-};
