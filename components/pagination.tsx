c8e8d935bec0c48d16c3c7d898db490d18ee091a
幻非 <eg1981400335@gmail.com>
2023-07-14 00:21:20 +0800

    update ts

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -2,34 +2,27 @@ import Link from 'next/link';
 
 const buttonStyle = 'rounded-xl bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 shadow';
 
-interface Pagination {
-    current: number;
-    total?: number;
-}
-
-export function Prev({ current }: Pagination) {
-    if (current === 1) return <div></div>;
-    return (
-        <Link href={current === 2 ? '/' : `/page/${current - 1}`} className={buttonStyle}>
-            上一页
-        </Link>
-    );
-}
-
-export function Next({ current, total }: Pagination) {
-    if (current === total) return <div></div>;
-    return (
-        <Link href={`/page/${current + 1}`} className={buttonStyle}>
-            下一页
-        </Link>
-    );
-}
-
-export default function Pagination({ current, total }: Pagination) {
+export default function Pagination({ current, total }: { current: number; total: number }) {
+    function Prev() {
+        if (current === 1) return <div></div>;
+        return (
+            <Link href={current === 2 ? '/' : `/page/${current - 1}`} className={buttonStyle}>
+                上一页
+            </Link>
+        );
+    }
+    function Next() {
+        if (current === total) return <div></div>;
+        return (
+            <Link href={`/page/${current + 1}`} className={buttonStyle}>
+                下一页
+            </Link>
+        );
+    }
     return (
         <nav className="mt-6 flex items-center justify-between">
-            <Prev current={current} />
-            <Next current={current} total={total} />
+            <Prev />
+            <Next />
         </nav>
     );
 }
