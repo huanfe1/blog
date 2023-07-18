1d5f9dac03a42ffeaa3847af10609f5b85deaba6
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-07-19 00:10:49 +0800

    update posts api [skip vercel]

--- pages/api/posts/create.ts
+++ pages/api/posts/create.ts
@@ -2,52 +2,37 @@ import fs from 'fs';
 import { allPosts } from '@/.contentlayer/generated';
 import crc32 from '@/utils/crc32';
 import dayjs from 'dayjs';
+import fm from 'gray-matter';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const abbrlinks = new Set([...allPosts.map(post => post.abbrlink)]);
 
-const getFilePath = (title: string, num: number = 0): string => {
+function getFilePath(title: string, num: number = 0): string {
     const path = `./articles/drafts/${title}${num === 0 ? '' : '-' + num}.md`;
     if (fs.existsSync(path)) {
         return getFilePath(title, num + 1);
     }
     return path;
-};
+}
+
+function getAbbrlink(title: string): string {
+    const abbrlink = crc32(title);
+    if (abbrlinks.has(abbrlink)) return getAbbrlink(title + abbrlink);
+    return abbrlink;
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (process.env.NODE_ENV !== 'development') return;
-    const hash = (title: string, attempt: number = 0) => {
-        const abbrlink = crc32(title);
-        if (abbrlinks.has(abbrlink)) {
-            return hash(title + abbrlink, attempt + 1);
-        }
-        return abbrlink;
-    };
-    const title = req.query.title[0];
-    const abbrlink = hash(title);
+    const title = req.query.title as string;
+    if (!title) return res.status(200).json({ code: 1, message: 'title is required' });
+    const abbrlink = getAbbrlink(title);
     abbrlinks.add(abbrlink);
     const frontmatter = {
-        title: title,
+        title,
         date: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-        abbrlink: abbrlink,
-        author: '幻非',
-        cover: '',
-        comments: '',
-        tags: '',
-        copyright: '',
-        categories: '',
+        abbrlink,
     };
-    let content = '---\n';
-    for (const [key, value] of Object.entries(frontmatter)) {
-        content += `${key}: ${value}\n`;
-    }
-    content += '---\n';
-    try {
-        const filePath = await getFilePath(title.replace(/\s+/g, '-'));
-        await fs.promises.writeFile(filePath, content);
-    } catch (e) {
-        res.status(200).json({ code: 1, message: e.message });
-    } finally {
-        res.status(200).json({ code: 0, message: 'success' });
-    }
+    const filePath = getFilePath(title.replace(/\s+/g, '-'));
+    fs.writeFileSync(filePath, fm.stringify('', frontmatter), 'utf-8');
+    return res.status(200).json({ code: 0, message: abbrlink });
 }
