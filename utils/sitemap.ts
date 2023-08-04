2f813f8802bd02aebd1405a320de538b1352968d
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2023-08-04 12:26:39 +0800

    update sitemap

--- utils/sitemap.ts
+++ utils/sitemap.ts
@@ -1,37 +1,20 @@
-import fs from 'fs';
 import { allPosts } from '@/.contentlayer/generated';
 import dayjs from 'dayjs';
-
-allPosts.sort((a, b) => dayjs(b.date).unix() - dayjs(a.date).unix());
-const url: string = 'https://blog.huanfei.top';
-
-function xml() {
-    const header = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    const posts = allPosts
-        .filter(post => !post.draft)
-        .map(post =>
-            [
-                '<url>',
-                `<loc>${url}/post/${post.abbrlink}</loc>`,
-                `<lastmod>${dayjs(post.date).toISOString()}</lastmod>`,
-                '</url>',
-            ].join('')
-        )
-        .join('');
-    const other = ['/about', '/archive'].map(_ => `<url><loc>${url + _}</loc></url>`).join('');
-    const content = [posts, other].join('');
-    return [header, content, '</urlset>'].join('');
-}
-
-const txt: string = allPosts
-    .filter(post => !post.draft)
-    .map(post => `${url}/post/${post.abbrlink}`)
-    .join('\n');
-
-const generatedTxt = [url, txt, url + '/about', url + '/archive'].join('\n');
+import fs from 'fs';
+import { streamToPromise, SitemapStream } from 'sitemap';
 
 export default function Sitemap() {
-    // const content = [header, ...posts].join('') + '</urlset>';
-    fs.writeFileSync('./public/sitemap.xml', xml());
-    fs.writeFileSync('./public/sitemap.txt', generatedTxt);
+    const sitemap = new SitemapStream({
+        hostname: 'http://blog.huanfei.top',
+        xmlns: { news: false, xhtml: false, image: false, video: false },
+    });
+    sitemap.write({ url: '/' });
+    allPosts.sort((a, b) => dayjs(b.date).unix() - dayjs(a.date).unix());
+    allPosts
+        .filter(post => !post.draft)
+        .map(post => sitemap.write({ url: `/post/${post.abbrlink}`, lastmod: post.date }));
+    sitemap.write({ url: '/about' });
+    sitemap.write({ url: '/archive' });
+    sitemap.end();
+    streamToPromise(sitemap).then(buffer => fs.writeFileSync('./public/sitemap.xml', buffer.toString()));
 }
