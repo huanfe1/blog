8595947f87f3b5dfc0587895ba09a5274829c36b
幻非 <eg1981400335@gmail.com>
2023-10-03 15:33:46 +0800

    更新界面

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
 import Analytics from '@/components/analytics';
 import { Head, Html, Main, NextScript } from 'next/document';
 
-const theme = `(() => {void 0 === localStorage.theme && localStorage.setItem('theme', 'auto');document.documentElement.classList.add(localStorage.theme);})();`;
+// 黑暗模式
+// const theme = `(() => {void 0 === localStorage.theme && localStorage.setItem('theme', 'auto');document.documentElement.classList.add(localStorage.theme);})();`;
 
 export default function Document() {
     return (
@@ -12,7 +13,7 @@ export default function Document() {
                 <link rel="icon" href="https://blog.huanfei.top/avatar.png" type="image/png" sizes="96x96" />
             </Head>
             <body>
-                <script dangerouslySetInnerHTML={{ __html: theme }} />
+                {/* <script dangerouslySetInnerHTML={{ __html: theme }} /> */}
                 <Main />
                 <NextScript />
             </body>
