0b220d53a2da5ff2098e44ed70c9fff94ac064b6
幻非 <eg1981400335@gmail.com>
2023-10-03 15:33:46 +0800

    更新界面

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 export default function footer() {
     return (
-        <footer className="mx-auto flex w-full justify-center border-t border-[--border] text-center text-sm text-gray-500 xl:max-w-7xl">
+        <footer className="mx-auto flex w-full justify-center bg-white text-center text-sm text-black shadow-sm">
             <span className="py-10">{`© 2022 - ${dayjs().format('YYYY')} All rights reserved.`}</span>
         </footer>
     );
