c6bde60d4aae6890961f01e02da685f3d24623bc
幻非 <eg1981400335@gmail.com>
2023-12-31 22:29:33 +0800

    样式优化

--- components/top.tsx
+++ components/top.tsx
@@ -6,7 +6,7 @@ export default function Top() {
         history.replaceState(null, null, ' ');
     };
     return (
-        <Button isIconOnly className="fixed bottom-10 right-10" size="lg" onPress={click}>
+        <Button isIconOnly className="fixed bottom-10 right-10 z-50 hidden md:flex" size="lg" onPress={click}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6l1.41 1.41z" />
             </svg>
