496db8335585658853a075e4bb7bef23964a58d0
幻非 <eg1981400335@gmail.com>
2024-01-12 21:10:01 +0800

    fix bug

--- utils/cache.ts
+++ utils/cache.ts
@@ -1,15 +1,18 @@
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-export function Cache(cachePath: string) {
-    const cacheDirectory = join(cachePath, '/');
-    !existsSync(cacheDirectory) && !process.env.VERCEL_REGION && mkdirSync(cacheDirectory, { recursive: true });
+const IsVercel: boolean = !!process.env.VERCEL_REGION;
+const cacheDirectory = process.env.TEMP_DIR || '.temp';
+export function cache() {
+    if (!IsVercel && !existsSync(cacheDirectory)) mkdirSync(cacheDirectory, { recursive: true });
     return {
         get: (name: string): any => {
+            if (IsVercel) return;
             const filePath = join(cacheDirectory, `${name}.json`);
             return existsSync(filePath) ? JSON.parse(readFileSync(filePath, 'utf-8')) : null;
         },
         set: (name: string, data: any): void => {
+            if (IsVercel) return;
             writeFileSync(join(cacheDirectory, `${name}.json`), JSON.stringify(data));
         },
     };
