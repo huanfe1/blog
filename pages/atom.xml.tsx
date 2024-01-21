6dab06c4ce4db5642a61a7a3c9c592042eaeac72
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-01-21 23:27:21 +0800

    update

--- pages/atom.xml.tsx
+++ pages/atom.xml.tsx
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 import { Feed } from 'feed';
 import { GetServerSideProps } from 'next';
 
-const url = 'https://blog.huanfei.top';
+const url = 'https://www.huanfei.top';
 
 const feed = new Feed({
     title: '幻非',
@@ -16,7 +16,7 @@ const feed = new Feed({
     copyright: `Copyright © 2022 - ${dayjs().format('YYYY')} HuanFei All Rights Reserved`,
     author: {
         name: '幻非',
-        link: 'https://huanfei.top',
+        link: 'https://www.huanfei.top',
     },
     favicon: `${url}/favicon.ico`,
     generator: 'Nexj.js + Notion',
@@ -36,6 +36,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
         });
     });
     res.setHeader('Content-Type', 'text/xml');
+    res.setHeader('Cache-Control', 'public, s-maxage=1200, stale-while-revalidate=600');
     res.write(feed.atom1());
     res.end();
     return { props: {} };
