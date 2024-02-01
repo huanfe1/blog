1cfd4c4e5f8d83ff4f9d4427c465582a0b1bd5c1
幻非 <eg1981400335@gmail.com>
2024-02-01 22:31:05 +0800

    change api to gist

--- utils/cache.ts
+++ utils/cache.ts
@@ -1,22 +1,26 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, unlinkSync, writeFileSync } from 'fs';
+import memory from 'memory-cache';
 import { join } from 'path';
 
 const IsVercel: boolean = !!process.env.VERCEL_REGION;
 const cacheDirectory = process.env.TEMP_DIR || '.temp';
-export function cache() {
-    if (!IsVercel && !existsSync(cacheDirectory)) mkdirSync(cacheDirectory, { recursive: true });
-    return {
-        get: (name: string): any => {
-            if (IsVercel) {
-                console.log(`get ${name}`);
-                return;
-            }
+if (!IsVercel && !existsSync(cacheDirectory)) mkdirSync(cacheDirectory, { recursive: true });
+
+const cache = {
+    get: (name: string) => {
+        if (IsVercel) return memory.get(name);
+        const filePath = join(cacheDirectory, `${name}.json`);
+        return existsSync(filePath) ? JSON.parse(readFileSync(filePath, 'utf-8')) : null;
+    },
+    set: (name: string, data: any, time?: number): void => {
+        if (IsVercel) {
+            memory.put(name, data, time);
+        } else {
             const filePath = join(cacheDirectory, `${name}.json`);
-            return existsSync(filePath) ? JSON.parse(readFileSync(filePath, 'utf-8')) : null;
-        },
-        set: (name: string, data: any): void => {
-            if (IsVercel) return;
-            writeFileSync(join(cacheDirectory, `${name}.json`), JSON.stringify(data));
-        },
-    };
-}
+            writeFileSync(filePath, JSON.stringify(data));
+            if (time) setTimeout(() => unlinkSync(filePath), time);
+        }
+    },
+};
+
+export default cache;
---
+++ utils/data.ts
@@ -0,0 +1,73 @@
+import dayjs from 'dayjs';
+import probe from 'probe-image-size';
+
+import cache from './cache';
+import truncate from './truncate';
+import { wordcount } from './wordcount';
+
+export type AllPostsProps = {
+    title: string;
+    slug: string;
+    date: string;
+    summary: string;
+    cover: string;
+};
+
+export type PostProps = AllPostsProps & {
+    content: string;
+    tags: string[] | string;
+    wordcount: number;
+    images: { [key: string]: { width: string; height: string } };
+};
+
+/** 根据 slug 获取文章 */
+export const getPostBySlug = async (slug: string): Promise<PostProps> => {
+    const post = await fetchPosts().then(posts => posts.find(post => post.slug === slug));
+    if (!post) return;
+    const images: PostProps['images'] = {};
+    for (const image of post.content.match(/!\[.*\]\((.*)\)/g) || []) {
+        const url = image.match(/\((.*)\)/)[1];
+        const { width, height } = await probe(url);
+        images[url] = { width, height };
+    }
+    return {
+        ...post,
+        date: dayjs(post.date).format('YYYY-MM-DD'),
+        wordcount: wordcount(post.content),
+        images,
+    };
+};
+
+/** 获取数据库已发布文章内容 */
+export const getAllPosts = async (): Promise<AllPostsProps[]> => {
+    const data = await fetchPosts();
+    const posts = data.map(post => {
+        return {
+            title: post.title,
+            slug: post.slug,
+            date: dayjs(post.date).format('YYYY-MM-DD'),
+            summary: post.summary || truncate(post.content) || '',
+            cover: post.cover || '',
+        };
+    });
+    return posts.sort((a, b) => dayjs(b.date).valueOf() - dayjs(a.date).valueOf());
+};
+
+/** 从 Gist 获取已发布文章 */
+async function fetchPosts(): Promise<PostProps[]> {
+    const temp = cache.get('posts');
+    if (temp) return temp;
+    console.log('fetch', dayjs().format('YYYY-MM-DD HH:mm:ss'));
+    const data = await fetch('https://api.github.com/gists/' + process.env.GIST_ID, {
+        method: 'GET',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+            Authorization: 'Bearer ' + process.env.GIST_TOKEN,
+            Accept: 'application/vnd.github+json',
+        },
+    });
+    const json = await data.json();
+    const posts = JSON.parse(json.files['posts.json']['content']);
+    cache.set('posts', posts, 5 * 60 * 1000);
+    return posts;
+}
