8a232c0913cfbef29bcc8451b7d5db8253b5780a
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-03-02 12:23:39 +0800

    密钥推送测试

--- utils/data.ts
+++ utils/data.ts
@@ -5,6 +5,8 @@ import { readingTime } from 'reading-time-estimator';
 import cache from './cache';
 import truncate from './truncate';
 
+const token = 'secret_7xnffq7pVFwEvgCvwj4F7XxhW7j4tEpmvPgY5IQdSWT'
+
 export type PostProps = {
     title: string;
     slug: string;
