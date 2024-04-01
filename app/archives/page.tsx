1bfb2e25a12d3dbda0e397ce09d193139388135d
huanfe1 <eg1981400335@gmail.com>
2024-04-01 22:35:33 +0800

    change: archives

--- app/archives/page.tsx
+++ app/archives/page.tsx
@@ -2,73 +2,47 @@ import { Card, CardBody } from '@nextui-org/card';
 import dayjs from 'dayjs';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import { JSX } from 'react';
 
 import { type PostProps, getAllPosts } from '@/utils/data';
 
 export const metadata: Metadata = { title: '归档页' };
 
 export default async function Archives() {
-    const posts = await getAllPosts();
-    const archives: Archive[] = getArchives(posts);
-    const length = posts.length;
+    const posts: PostProps[] = await getAllPosts();
+    posts.sort((a, b) => dayjs(b.date).valueOf() - dayjs(a.date).valueOf());
+    const result: JSX.Element[] = [];
+    let temp = '';
+    for (const post of posts) {
+        const year = dayjs(post.date).format('YYYY');
+        if (year !== temp) {
+            result.push(
+                <h2 key={year} className="text-3xl font-bold">
+                    {year}
+                </h2>,
+            );
+            temp = year;
+        }
+        result.push(
+            <Link href={'/post/' + post.slug} key={post.slug} className="block">
+                <Card className="w-full bg-content2 hover:text-primary">
+                    <CardBody className="flex flex-row items-center justify-between px-4 py-4">
+                        <h3 className="truncate text-base md:text-lg">{post.title}</h3>
+                        <time dateTime={post.date} title={post.date} className="ml-3 flex-none">
+                            {dayjs(post.date).format('MM-DD')}
+                        </time>
+                    </CardBody>
+                </Card>
+            </Link>,
+        );
+    }
     return (
-        <>
+        <div className="resp mb-20">
             <div className="my-28 flex flex-col items-center justify-center rounded-2xl">
                 <h1 className="text-5xl font-bold">归档</h1>
-                <div className="mt-5">{`${length} 篇文章`}</div>
+                <div className="mt-5">{`${posts.length} 篇文章`}</div>
             </div>
-            <ul className="resp mb-20 space-y-10">
-                {archives.map(categorie => (
-                    <li key={categorie.year}>
-                        <h2 className="pb-6 text-3xl font-bold">{categorie.year}</h2>
-                        <div className="space-y-8">
-                            {categorie.posts.map(post => (
-                                <Link href={'/post/' + post.slug} key={post.slug} className="block">
-                                    <Card className="w-full bg-content2 hover:text-primary">
-                                        <CardBody className="flex flex-row items-center justify-between px-4 py-4">
-                                            <h3 className="truncate text-base md:text-lg">{post.title}</h3>
-                                            <time dateTime={post.date} title={post.date} className="ml-3 flex-none">
-                                                {dayjs(post.date).format('MM-DD')}
-                                            </time>
-                                        </CardBody>
-                                    </Card>
-                                </Link>
-                            ))}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </>
+            <div className="space-y-8">{result}</div>
+        </div>
     );
-}
-
-type Archive = {
-    year: string;
-    posts: { title: string; date: string; slug: string }[];
-};
-
-function getArchives(posts: PostProps[]) {
-    const arr: Archive[] = [];
-    posts.forEach(post => {
-        const year = dayjs(post.date).format('YYYY');
-        const index = arr.findIndex(item => item.year === year);
-        const temp = {
-            title: post.title,
-            date: dayjs(post.date).format('YYYY-MM-DD'),
-            slug: post.slug,
-        };
-        if (index !== -1) {
-            arr[index].posts.push(temp);
-        } else {
-            arr.push({
-                year,
-                posts: [temp],
-            });
-        }
-    });
-    arr.sort((a, b) => parseInt(b.year) - parseInt(a.year));
-    arr.forEach(item => {
-        item.posts.sort((a, b) => parseInt(b.date) - parseInt(a.date));
-    });
-    return arr;
 }
