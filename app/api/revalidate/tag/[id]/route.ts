929e48bcc91740a2229fbd0bc7b132c1b4350cc7
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-04-14 18:10:33 +0800

    Update

--- app/api/revalidate/tag/[id]/route.ts
+++ app/api/revalidate/tag/[id]/route.ts
@@ -1,6 +1,6 @@
 import { revalidateTag } from 'next/cache';
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-    revalidateTag(params.id);
+    setTimeout(() => revalidateTag(params.id), 30000);
     return Response.json({ revalidated: true, now: Date.now() });
 }
