268aca03925b2329c5a083739c5c89cb0fee0772
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-05-25 16:06:43 +0800

    refactor: crlf to lf

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 export default function footer() {
     return (
-        <footer className="flex items-center justify-around border-content2 py-12 pt-8 text-sm duration-250">
+        <footer className="border-content2 duration-250 flex items-center justify-around py-12 pt-8 text-sm">
             <span>{`© 2022 - ${dayjs().format('YYYY')} All rights reserved.`}</span>
         </footer>
     );
