cb76d7f6eece4ec85d4d229cf8cce62763cb9f9b
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-10-05 15:16:03 +0800

    feat: change svg to tailwind icon

--- components/layout/top.tsx
+++ components/layout/top.tsx
@@ -10,9 +10,7 @@ export default function Top() {
             className="fixed bottom-10 right-10 z-20 hidden cursor-pointer rounded-xl p-1 md:flex"
             onClick={click}
         >
-            <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6l-6 6l1.41 1.41z" />
-            </svg>
+            <span className="i-mingcute-up-line text-3xl"></span>
         </div>
     );
 }
