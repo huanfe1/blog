626cc68abe5826cda5e83832b46ff58c281d1729
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2024-11-02 23:14:53 +0800

    style: update post header style
    [skip vercel]

--- app/post/[id]/header.tsx
+++ app/post/[id]/header.tsx
@@ -6,12 +6,12 @@ export default function Header({ post }: { post: PostProps }) {
     return (
         <header>
             {post.cover && (
-                <div className="mb-5 aspect-video overflow-hidden rounded shadow">
+                <div className="mb-8 aspect-video overflow-hidden rounded shadow">
                     <img src={post.cover} alt={post.title} />
                 </div>
             )}
             <h1 className="text-3xl font-bold">{post.title}</h1>
-            <div className="my-2 opacity-60">
+            <div className="my-3 opacity-60">
                 <time dateTime={post.date}>{post.date}</time>
                 <span className="mx-1">·</span>
                 <span>{'约 ' + readingTime(post.content, 300, 'cn').words + ' 字'}</span>
