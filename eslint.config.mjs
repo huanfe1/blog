cb85dbad7be041927f687d87f7a0ccfec0c68853
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2025-05-27 22:08:55 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,62 +1,23 @@
-import js from '@eslint/js';
-import nextPlugin from '@next/eslint-plugin-next';
-import typescript from '@typescript-eslint/eslint-plugin';
-import parserTypescript from '@typescript-eslint/parser';
-import configPrettier from 'eslint-config-prettier';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import globals from 'globals';
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-/** @type { import('eslint').Linter.Config[] } */
-const config = [
-    {
-        ignores: ['**/node_modules', 'pnpm-lock.yaml', '.next', '.next-env.d.ts', '.turbo', '.output'],
-    },
-    {
-        languageOptions: {
-            ecmaVersion: 2022,
-            globals: { ...globals.browser, ...globals.es2021, ...globals.node },
-        },
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.config({
+        extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
         rules: {
-            ...js.configs.recommended.rules,
-        },
-    },
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        plugins: {
-            react: reactPlugin,
-            'react-hooks': reactHooksPlugin,
-        },
-        rules: {
-            ...reactPlugin.configs['jsx-runtime'].rules,
-            ...reactHooksPlugin.configs.recommended.rules,
+            '@next/next/no-img-element': 'off',
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
             'react-hooks/exhaustive-deps': 'off',
         },
-    },
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        plugins: { '@typescript-eslint': typescript },
-        languageOptions: { parser: parserTypescript },
-        rules: {
-            ...typescript.configs.recommended.rules,
-            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-            // '@typescript-eslint/no-explicit-any': ['warn'],
-        },
-    },
-    {
-        ...configPrettier,
-    },
-    {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-        plugins: {
-            '@next/next': nextPlugin,
-        },
-        rules: {
-            ...nextPlugin.configs.recommended.rules,
-            ...nextPlugin.configs['core-web-vitals'].rules,
-            '@next/next/no-img-element': 'off',
-        },
-    },
+    }),
 ];
 
-export default config;
+export default eslintConfig;
